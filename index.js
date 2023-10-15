278bae490f582f8481c53c722e03eb6e220f4ab5
dolininak <144163170+dolininak@users.noreply.github.com>
2023-10-15 22:00:43 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const outputNode=document.querySelector('.js-output');
 var konf=document.getElementById('konf');
 const inputs=document.querySelectorAll('input,select');
 const radioDost=document.querySelectorAll('input[name="dost"]');
+const chek2=document.querySelectorAll('input[name="check2"]');
 const check = document.getElementById('chek');
 const selector= document.getElementById('tipm');
 function calculate(elem0,elem){
@@ -11,18 +12,27 @@ function calculate(elem0,elem){
     totalPrice=cena*kol1;
     for (const radio of radioDost){
         if(radio.checked===true){
-            totalPrice+=parseInt(radio.value);
+            
             if (radio.value === '100') {
                 selector.style.display = 'none';
                 check.style.display = 'none';
+                totalPrice+=parseInt(radio.value);
             }
             else if(radio.value === '0'){
                 selector.style.display = 'block';
                 check.style.display = 'none';
+                totalPrice+=parseInt(radio.value);
+                totalPrice+=parseInt(selector.querySelector(':checked').getAttribute('data-price'));
             }
             else if(radio.value === '200'){
                 selector.style.display = 'none';
                 check.style.display = 'block'; 
+                totalPrice+=parseInt(radio.value);
+                for (const chek of chek2){
+                    if(chek.checked===true){
+                        totalPrice*=parseFloat(chek.value);
+                    }
+                }
             }
         }
     }
@@ -30,7 +40,7 @@ function calculate(elem0,elem){
 for(const input of inputs){
     input.addEventListener('input',function(){
         calculate(kol,konf);
-        var regex = /^\d+$/;
+        var regex = /^\d+(\.\d+)?$/;
         if (!regex.test(totalPrice)) {
             outputNode.innerHTML = "Недопустимые символы";
           } else {
